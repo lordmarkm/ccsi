242020e8df7b55542f98caf125e3ed4f5a6c91c8
mbmartinez <mbmartinez@exist.com>
2014-09-30 18:09:52 +0800

    Delete target/

--- ccsi-web/target/ccsi-web-1.0/tenant/controllers/controllers.js
+++
@@ -1,8 +0,0 @@
-define([
-  'tenant/controllers/AuthController.js',
-  'tenant/controllers/TenantCreateController.js',
-  'tenant/controllers/TenantHomeController.js',
-  'tenant/controllers/TenantController.js',
-  'tenant/controllers/TenantTemplatesController.js',
-  'tenant/controllers/TenantTransactionsController.js'
-], function () {});
