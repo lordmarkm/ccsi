d182f4a688f0170dcee159b121648fd0b8d6857c
Markm <lordmarkm@gmail.com>
2014-12-06 13:45:47 +0800

    ggg

--- ccsi-web/src/main/webapp/tenant/controllers/MapController.js
+++ ccsi-web/src/main/webapp/tenant/controllers/MapController.js
@@ -1,9 +1,11 @@
-define(['tenant/controllers/module.js'], function (controllers) {
+define(['angular', 'tenant/controllers/module.js'], function (angular, controllers) {
   'use strict';
   controllers.controller('MapController', ['$scope', '$state',
     function($scope, $state) {
 
+
     $scope.name = 'MapController';
     $scope.whatever = {name: 'helloworld'};
 
-  });
+  }]);
+});
